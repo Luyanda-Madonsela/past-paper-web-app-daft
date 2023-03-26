e78bc4738933c8a3795504b369fd0752ade764e6
Luyanda Madonsela <luyandasmadonsela@gmail.com>
2023-03-26 05:59:27 +0200

    UI Additions
    UI for sign in, sign up and contact pages

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,9 +1,116 @@
+"use client"
 import styles from "./page.module.css";
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Image from 'next/Image';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import message from '/public/images/message.gif'
 
-export default function Home() {
+    const sendButt = {
+        backgroundColor: '#6EC5EB',
+        color: 'white',
+    };
+
+    export default function Home()
+    {
+        const handleSubmit = (event) => {
+            event.preventDefault();}
+            const data = new FormData(Event.currentTarget);
+            console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+            });
+
 return (
     <div className={styles.main}>
-        <h1>Contact Us</h1>
+        <div className={styles.mainSection1}>
+
+                <Image src={message}  alt='/' layout="intrinsic"
+                    sizes="(max-width: 768px) 100vw,
+                    (max-width: 1200px) 50vw,
+                    33vw" 
+                    width={100} 
+                    height={40}
+                />
+
+        </div>
+
+        <div className={styles.mainSection2}>
+
+            <div className={styles.mainSection2a}>
+                <p className={styles.header}>Leave a message</p>
+
+                <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+
+                <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="fullName"
+                        label="Full Name"
+                        name="fullName"
+                        autoComplete="fullName"
+                        autoFocus
+                    />
+
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                    />
+
+                    <TextField
+                        margin="normal"
+                        fullWidth
+                        name="phone"
+                        label="Phone (optional)"
+                        type="phone"
+                        id="phone"
+                        autoComplete="phone"
+                    />
+
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="subject"
+                        label="Message Subject"
+                        type="subject"
+                        id="subject"
+                        autoComplete="subject"
+                    />
+
+                    <TextField
+                    required
+                    id="outlined-textarea"
+                    label="Message"
+                    placeholder="Type your message here..."
+                    multiline
+                    minRows={6}
+                    fullWidth
+                    />
+
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                        style={sendButt}
+                    >
+                    Send Message
+                    </Button>
+
+                </Box>
+
+                </div>
+
+        </div>
+        <div className={styles.mainSection3}></div>
     </div>
 )
 }
